168d149cad0f2cc7a5f768ce511ae36811b58d5b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-03 10:27:16 +0000

    Update font

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,11 @@
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
             @select="handleSelect">
             <el-menu-item index="0">
-                <NuxtLink to="/">APOKON</NuxtLink>
+                <NuxtLink to="/">
+                    <span class="font-semibold">
+                        Apokon Elementary School
+                    </span>
+                </NuxtLink>
             </el-menu-item>
             <div class="flex-grow" />
             <el-menu-item index="1">
