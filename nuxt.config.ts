256e6b9ec6c1f67f12e97f86e75fd577ed68212e
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-03 17:21:50 +0800

    Set target to static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,19 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css", "element-plus/dist/index.css"],
+
+  target: "static",
+
+  components: true,
+
   modules: ["@nuxtjs/tailwindcss"],
+
+  build: {
+    transpile: ["element-plus"],
+    splitChunks: {
+      layouts: true,
+      pages: true,
+      commons: true,
+    },
+  },
 });
