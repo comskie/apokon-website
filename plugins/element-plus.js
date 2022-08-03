8c9d582a31575964b14810266d6d76784660af44
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-03 17:22:06 +0800

    Remove ID_INJECTION_KEY

--- plugins/element-plus.js
+++ plugins/element-plus.js
@@ -2,8 +2,4 @@ import ElementPlus, { ID_INJECTION_KEY } from 'element-plus';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(ElementPlus);
-    nuxtApp.vueApp.provide(ID_INJECTION_KEY, {
-        current: 100,
-        prefix: 0,
-    });
 })
