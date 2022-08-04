7daefc42e55e9148f2602b862b653afa89b7f7e2
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-05 00:31:11 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,6 @@
 <template>
   <div>
-    <nav class="p-4 border-b">
-      <div class="flex flex-row justify-between">
-        <div class="font-bold">
-          <NuxtLink to="/">APOKON</NuxtLink>
-        </div>
-        <div class="grid grid-flow-col gap-2">
-          <NuxtLink to="/history">History</NuxtLink>
-          <NuxtLink to="/admissions">Admissions</NuxtLink>
-        </div>
-      </div>
-      <div></div>
-    </nav>
+   <header-layout />
     <slot />
   </div>
 </template>
-
-<script lang="ts" setup>
-import { ref } from "vue";
-
-const activeIndex = ref("1");
-const handleSelect = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath);
-};
-</script>
