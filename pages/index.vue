69a9f4502ba1b20967079a3249ed4bb59685fddb
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-05 00:30:39 +0800

    Remove carousel

--- pages/index.vue
+++ pages/index.vue
@@ -8,29 +8,17 @@
     </div>
     <div class="col-span-2">
       <span>Our School</span>
-      <el-carousel trigger="click" class="rounded-md">
-        <el-carousel-item v-for="item in 4" :key="item">
-          <h3 class="small justify-center" text="2xl">{{ item }}</h3>
-        </el-carousel-item>
-      </el-carousel>
+      <div>carousel</div>
     </div>
   </div>
 </template>
 
-<style>
-.el-carousel__item h3 {
-  color: #475669;
-  opacity: 0.75;
-  line-height: 150px;
-  margin: 0;
-  text-align: center;
-}
+<script lang="ts" setup>
+import "animate.css";
 
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
+import { useHead } from "#imports";
 
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
-}
-</style>
+useHead({
+  title: "Home",
+});
+</script>
