fc9949bde96cd8964b91f302ec5921388364e285
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-04 10:30:39 +0800

    Create nav-link.vue

---
+++ components/nav-link.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+defineProps({
+  to: {
+    required: true,
+    type: String,
+  },
+});
+
+</script>
+
+<template>
+  <div>
+    <NuxtLink :to="to">
+      <slot />
+    </NuxtLink>
+  </div>
+</template>
