09113d04ceb5f53ec4dfa014779eaa10ad6b684d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-05 18:32:52 +0800

    Use nav-link.vue

--- components/nav-link.vue
+++ components/nav-link.vue
@@ -9,14 +9,19 @@ defineProps({
     default: false,
   },
 });
+
 </script>
 
 <template>
   <NuxtLink
-    class=" text-sm tracking-wide p-2 block sm:inline-flex items-center justify-center h-full hover:bg-blue-200/50 focus:bg-none"
-    :class="{
-    'focus:bg-blue-200/50 border-r-2 sm:border-r-0 sm:border-b-2 border-blue-500 text-blue-500': isActive
-  }" :to="to">
-    <slot/>
+    class="sm:border-b-2 text-sm tracking-wide p-2 block sm:inline-flex items-center justify-center h-full hover:bg-blue-200/50 focus:bg-none"
+    :class="[
+      isActive
+        ? 'focus:bg-blue-200/50 border-r-2 sm:border-r-0 sm:border-b-2 border-blue-500 text-blue-500'
+        : 'sm:border-transparent',
+    ]"
+    :to="to"
+  >
+    <slot />
   </NuxtLink>
 </template>
