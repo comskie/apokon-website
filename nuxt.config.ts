ade6a6dd89c058716f052c06dec785993a99aef4
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-05 10:14:41 +0800

    Remove unused variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 import { defineNuxtConfig } from "nuxt";
 
-const lifecycle = process.env.npm_lifecycle_event;
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
