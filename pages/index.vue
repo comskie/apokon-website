6643861927be37958b48da23ff5d9bde243555ee
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-05 19:26:21 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,7 @@
 <template>
-  <div class="grid grid-cols-3 gap-4 p-4">
-    <div
-      class="bg-slate-400 rounded-md flex gap-4 items-center justify-center flex-col p-4"
-    >
-      <div class="rounded-full h-52 w-52 bg-lime-500"></div>
-      <p class="font-bold">Apokon Elementary School</p>
-    </div>
-    <div class="col-span-2">
-      <span>Our School</span>
-      <div>carousel</div>
-    </div>
+  <div>
+    <carousel />
+    <h1 class="text-3xl font-bold">Our School</h1>
   </div>
 </template>
 
