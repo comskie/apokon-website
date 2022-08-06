67de967210d1ae404697a1e9bb2877378ee03178
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-06 23:21:32 +0800

    Improve carousel indicator

--- components/carousel.vue
+++ components/carousel.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
-// const images = [
-//   "https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg",
-//   "https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg",
-//   "https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg",
-// ];
+const images = [
+  "https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg",
+  "https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg",
+  "https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg",
+];
 
-const images = randomImages(3);
+// const images = randomImages(3);
 
 const currentImageIndex = ref(0);
 
@@ -30,53 +30,48 @@ function previousImage() {
   currentImageIndex.value =
     (currentImageIndex.value - 1 + images.length) % images.length;
 }
+
+function isCurrentImageIndex(imageIndex: number) {
+  return currentImageIndex.value === imageIndex;
+}
 </script>
 
 <template>
-  <div class="relative w-full">
+  <div class="w-full">
     <!-- Indicators -->
-    <div class="absolute right-0 bottom-0 left-0 flex justify-center z-10">
-      <button
-        v-for="(item, index) in images"
-        :key="index"
-        @click="selectImage(index)"
-      >
-        Indicator
+    <div class="absolute p-0 right-0 bottom-0 left-0 mb-4 flex justify-center z-[1] gap-2 w-full">
+      <button v-for="(imageUrl, imageIndex) in images" :key="imageIndex" @click="selectImage(imageIndex)" :class="[
+        'w-7 h-1 border-y-[10px] box-content bg-white rounded-sm border-transparent bg-clip-padding',
+        {
+          'opacity-50 hover:scale-x-110 hover:opacity-75': !isCurrentImageIndex(imageIndex),
+          'scale-y-125': isCurrentImageIndex(imageIndex),
+        }
+      ]">
       </button>
     </div>
 
     <!-- Content -->
-    <div class="relative">
-      <img
-        v-for="(imageUrl, imageIndex) in images"
-        :key="imageIndex"
-        :src="imageUrl"
-        :alt="imageUrl"
-        class="absolute"
-        :class="[
-          {
-            hidden: imageIndex !== currentImageIndex,
-          },
-        ]"
-      />
+    <div class="w-full relative overflow-hidden">
+      <div v-for="(imageUrl, imageIndex) in images" :key="imageIndex" :class="[
+        'w-full',
+        {
+          'hidden': imageIndex !== currentImageIndex,
+        },
+      ]">
+        <img :src="imageUrl" :alt="imageUrl" class="block w-full" />
+      </div>
     </div>
 
     <!-- Prev button -->
-    <button @click="nextImage()" class="z-10">
+    <button @click="previousImage()" class="z-10">
       <svg class="h-6 w-6" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"
-        />
+        <path fill="currentColor" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
       </svg>
     </button>
     <!-- Next button -->
     <button @click="nextImage()" class="z-10">
       <svg class="h-6 w-6" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
-        />
+        <path fill="currentColor" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
       </svg>
     </button>
   </div>
