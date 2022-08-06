8a9565cf330b2ce5f750cfde62aaab2a9c3807cd
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-06 23:20:45 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    <carousel />
-    <div class="mt-4 flex items-center">
-      <h1 class="text-3xl font-bold">Our School</h1>
-    </div>
+
+  <carousel />
+
+  <div class="mt-4 flex items-center">
+    <h1 class="text-3xl font-bold">Our School</h1>
   </div>
+
 </template>
 
 <script lang="ts" setup>
