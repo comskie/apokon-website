edcbf6e443f4ed6a7979e1c983a06a5a19026952
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-07 08:56:02 +0800

    Update carousel.vue

--- components/carousel.vue
+++ components/carousel.vue
@@ -37,9 +37,9 @@ function isCurrentImageIndex(imageIndex: number) {
 </script>
 
 <template>
-  <div class="w-full">
+  <div class="relative w-full">
     <!-- Indicators -->
-    <div class="absolute p-0 right-0 bottom-0 left-0 mb-4 flex justify-center z-[1] gap-2 w-full">
+    <div class="absolute p-0 right-0 bottom-0 left-0 mb-4 flex justify-center gap-2 z-[2] box-border mx-[15%]">
       <button v-for="(imageUrl, imageIndex) in images" :key="imageIndex" @click="selectImage(imageIndex)" :class="[
         'w-7 h-1 border-y-[10px] box-content bg-white rounded-sm border-transparent bg-clip-padding',
         {
@@ -51,27 +51,31 @@ function isCurrentImageIndex(imageIndex: number) {
     </div>
 
     <!-- Content -->
-    <div class="w-full relative overflow-hidden">
+    <div class="w-full overflow-hidden block">
       <div v-for="(imageUrl, imageIndex) in images" :key="imageIndex" :class="[
         'w-full',
         {
           'hidden': imageIndex !== currentImageIndex,
         },
       ]">
-        <img :src="imageUrl" :alt="imageUrl" class="block w-full" />
+        <img :src="imageUrl" :alt="imageUrl" class="block w-full float-left" />
       </div>
     </div>
 
     <!-- Prev button -->
-    <button @click="previousImage()" class="z-10">
-      <svg class="h-6 w-6" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+    <button @click="previousImage()"
+      class="absolute z-[1] inset-y-0 left-0 w-[15%] flex items-center justify-center group hover:bg-gradient-to-r hover:from-black/50">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white opacity-50 group-hover:opacity-100" fill="none"
+        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
       </svg>
     </button>
     <!-- Next button -->
-    <button @click="nextImage()" class="z-10">
-      <svg class="h-6 w-6" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+    <button @click="nextImage()"
+      class="absolute z-[1] inset-y-0 right-0 w-[15%] flex items-center justify-center group hover:bg-gradient-to-l hover:from-black/50">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white opacity-50 group-hover:opacity-100" fill="none"
+        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
       </svg>
     </button>
   </div>
