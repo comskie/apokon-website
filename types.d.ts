ce47acf1452fef16adbf1070676e3feb66a7d752
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-11 13:06:53 +0000

    Add types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,10 @@
+type Post = {
+  id: number;
+  slug: string;
+  title: string;
+  content: string;
+  cover_image_url: string;
+  created_at: string;
+  updated_at: string;
+  published_at: string;
+};
