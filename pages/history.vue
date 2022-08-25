d90691ac7415fa7109be1e932974063772f05f87
Zhyr Narciso <znarciso720@gmail.com>
2022-08-25 14:43:52 +0800

    Add content in history page

--- pages/history.vue
+++ pages/history.vue
@@ -1,5 +1,60 @@
 <template>
-    <div>
-        history
-    </div>
+  <div class="flex justify-center">
+    <article class="prose text-justify">
+      <img src="/images/teachers.jpg" alt="">
+      <h1 class="text-center">APOKON ELEMENTARY SCHOOL</h1>
+      <h2>History</h2>
+      <p class="indent-4">
+        Apokon Elementary School is committed to provide quality basic education
+        through the concerted effort of the readers, parents and other
+        stakeholders. Our mission is for every student to reach their full
+        potential through rigorous teaching and learning in a child-friendly
+        school.
+      </p>
+      <p class="indent-4">
+        The school is one of the complete elementary schools of Tagum City
+        Division, Region XI. It is located along the highway connecting Davao
+        del Norte-Davao Oriental National Highway, passing Barangay Apokon,
+        Tagum City. This is located approximately 3 kilometers from the heart of
+        the city going east bound to Mati, Davao Oriental. It was established in
+        June 1946 with simple beginning with multi grades classes with forty
+        (40) pupils in Grade I and II, and twenty-six (26) pupils in grade III
+        and IV as the first enrollees.
+      </p>
+      <p class="indent-4">
+        This is the only public elementary school which is situated in Barangay
+        Apokon. It caters educational services to its locality with an immensely
+        diverse culture including Bisaya, Mandaya, Mansaka and Muslim students.
+      </p>
+      <p class="indent-4">
+        Speaking of belief and religion, almost 64.8 % of the student populace
+        belongs to Roman Catholic, 14. 2% are Muslims, 5 % is Iglesia ni Kristo
+        and 16 % comprise Christians of different denominations. Barangay Apokon
+        is rich with diverse ethnic group, the home of Mansaka Tribe where its
+        roots originated named the barangay “Apok” meaning mud the original
+        occupants comprising 38% of the population, 12 % belongs to Mandaya, 10%
+        for other tribes while the remaining 40% distributed to migrants coming
+        from the different part of the country.
+      </p>
+      <p class="indent-4">
+        Apokon Elementary School is one of the five public elementary school in
+        Tagum Central Cluster. It is three (3) kilometers away from the heart of
+        the city of Tagum. It has a total lot area of 8,650 sq meters. It is
+        situated at a strategic location because as its nearby existing public
+        and private high schools and college whereby majority of its graduates
+        pursue secondary education and higher learning namely: University of
+        Southeastern Philippines located in the eastern part, St. Lorenzo Ruiz
+        Academy in the western part, Tagum National Trade School in the
+        Southeastern part and North Davao College Foundation Inc.
+      </p>
+      <p class="indent-4">
+        In terms of school performance, Apokon Elementary School does not lay
+        behind from other school of Tagum City Division. In fact, some of its
+        pupils have garnered several awards in the local, school, division,
+        regional and national competition both academic and non-academic
+        aspects. This truly indicates that the school has nurtured its pupils a
+        well-rounded individual.
+      </p>
+    </article>
+  </div>
 </template>
