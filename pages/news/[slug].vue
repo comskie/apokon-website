5976cd3148ab51848af3a5ba5fb0199d3c2d80eb
Rey Vincent Canlas <canlas.reyvincent@gmail.com>
2022-08-26 15:40:18 +0800

    Revert "Remove news"
    This reverts commit 1322942227ed0f4ce30ea3c68c89e50bde8029d4.

---
+++ pages/news/[slug].vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+const res = await useFetch(
+  `https://apokon-cms.herokuapp.com/api/posts?filter[slug]=${route.params.slug}`
+);
+
+const { data: posts } = res.data.value as { data: Post[] };
+const post = posts[0];
+</script>
+
+<template>
+  <div class="px-4 py-12">
+    <div class="flex flex-col items-center justify-center w-full">
+      <img :src="post.cover_image_url" :alt="post.title" class="h-96" />
+      <div class="mt-4">
+        <article class="prose">
+          <h1>{{ post.title }}</h1>
+          <div v-html="post.content"></div>
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
