7270c4673ee8a23944a7d9311bd640e953b13481
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-28 05:06:37 +0000

    Change grid columns 2 from 3

--- components/image-grid.vue
+++ components/image-grid.vue
@@ -5,7 +5,7 @@ import "@fancyapps/ui/dist/fancybox.css";
 
 <template>
   <div class="container">
-    <div class="grid sm:grid-cols-6 grid-cols-3 gap-4">
+    <div class="grid sm:grid-cols-6 grid-cols-2 gap-4">
       <slot />
     </div>
   </div>
