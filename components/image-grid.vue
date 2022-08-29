0143a422f425f8154e2635a33e955a49457d5acc
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-29 13:22:21 +0800

    Move fancybox import from gallery pages to image-grid component

--- components/image-grid.vue
+++ components/image-grid.vue
@@ -1,3 +1,13 @@
+<script lang="ts" setup>
+import { Fancybox } from "@fancyapps/ui/dist/fancybox.esm";
+
+Fancybox.bind("[data-fancybox]", {
+  Toolbar: {
+    display: ["zoom", "fullscreen", "thumbs", "close"],
+  },
+});
+</script>
+
 <template>
   <div class="container">
     <div class="grid sm:grid-cols-6 grid-cols-3 gap-4">
