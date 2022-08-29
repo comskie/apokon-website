e0c3a108378ebc04c7ea2ed9c1b1494375feebf0
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-29 13:13:14 +0800

    Add fancybox.css in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/assets/css/main.css"],
+  css: ["@/assets/css/main.css", "@fancyapps/ui/dist/fancybox.css"],
 
   modules: ["@nuxtjs/tailwindcss"],
 
